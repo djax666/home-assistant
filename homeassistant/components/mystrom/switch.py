07c6e4132f1a13b266882ba9f26f3caf6a614fab
djax666 <35227325+djax666@users.noreply.github.com>
2019-11-20 20:29:28 +0100

    Update switch.py
    Add the temperature report proposed by the version 2 of the MyStrom Switch

--- homeassistant/components/mystrom/switch.py
+++ homeassistant/components/mystrom/switch.py
@@ -62,6 +62,10 @@ class MyStromSwitch(SwitchDevice):
     def current_power_w(self):
         """Return the current power consumption in W."""
         return round(self.data["power"], 2)
+    
+    def current_temperature_c(self):
+        """Return the current temperature consumption in °C."""
+        return round(self.data["temperature"], 2)
 
     def turn_on(self, **kwargs):
         """Turn the switch on."""
@@ -88,5 +92,5 @@ class MyStromSwitch(SwitchDevice):
         try:
             self.data = self.plug.get_status()
         except exceptions.MyStromConnectionError:
-            self.data = {"power": 0, "relay": False}
+            self.data = {"power": 0, "relay": False, "temperature": 0}
             _LOGGER.error("No route to device: %s", self._resource)
